cc87ddb38bdba7a11f50c1d56189abb398f8a6ae
MoonSoon24 <sbstn.stp@gmail.com>
2025-08-08 07:38:00 +0700

    update14

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -65,9 +65,9 @@
                     </div>
                 </div>
 
-                <div x-data="{ dropdownOpen: false }" class="relative flex items-center">
-                    <button @click="dropdownOpen = !dropdownOpen" class="flex items-center space-x-2">
-                        <span class="hidden md:block font-semibold text-gray-700 hover:text-black transition">{{ Auth::user()->name }}</span>
+                <div x-data="{ dropdownOpen: false }" class="relative hidden md:block">
+                    <button @click="dropdownOpen = !dropdownOpen" class="flex items-center space-x-3">
+                        <span class="font-semibold text-gray-700 hover:text-black transition">{{ Auth::user()->name }}</span>
                         <div class="w-8 h-8"><svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-400" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" /></svg></div>
                     </button>
                     <div x-show="dropdownOpen" @click.away="dropdownOpen = false" class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50" x-cloak>
